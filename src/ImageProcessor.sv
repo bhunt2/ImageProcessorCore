714ef7459694721f97f24ff8e1913b031c9b17f6
Benjamin Huntsman <bhunt2@velocesolo.ece.pdx.edu>
2015-05-06 22:46:11 -0700

    adding files to work with veloce in standalone mode.

--- src/ImageProcessor.sv
+++ src/ImageProcessor.sv
@@ -7,12 +7,15 @@ import ImageProcessingPkg::*;
 
 module ImageProcessor(
 	input clk,
+	input rst,
 	input instruction_t IW,
 	output pixelMatrix_t result
 );
 
     // Always block for operating on the current input values
     always_comb begin
+		if (rst)
+			result = `b0;
         case (IW.opcode)
             ADD :   result = add(IW.cellA, IW.cellB);
         endcase
