833d4a78ab4875effe8fd938a1bb86285919b386
bhunt2 <bhunt2@pdx.edu>
2015-05-08 13:19:42 -0700

    changed veloce_top.sv to have logic vectors for inputs rather than 2D structs

--- src/veloce_top.sv
+++ src/veloce_top.sv
@@ -3,20 +3,20 @@ module veloce_top
 (
 	input  clk,
 	input  rst,
-    input  CellProcessingPkg::cell_t 		cellA,
-    input  CellProcessingPkg::cell_t 		cellB,
-    input  CellProcessingPkg::userInput_t 	userInputA,
-    input  CellProcessingPkg::opcodes_t 	opcode,
-    output CellProcessingPkg::cell_t		processedCell
+    input  [CellProcessingPkg::cellDepth - 1:0]	cellA,
+    input  [CellProcessingPkg::cellDepth - 1:0]	cellB,
+    input  CellProcessingPkg::userInput_t 		userInputA,
+    input  CellProcessingPkg::opcodes_t 		opcode,
+    output [CellProcessingPkg::cellDepth - 1:0]	processedCell
 );
 	
 	cellProcessor_int cell_int(clk, rst);
 
-	assign cell_int.cellA 		= cellA;
-	assign cell_int.cellB 		= cellB;
-	assign cell_int.userInputA 	= userInputA;
-	assign cell_int.opcode		= opcode;
-	assign processedCell		= cell_int.processedCell;
+	assign cell_int.cellA.singleCell	= cellA;
+	assign cell_int.cellB.singleCell	= cellB;
+	assign cell_int.userInputA 			= userInputA;
+	assign cell_int.opcode				= opcode;
+	assign processedCell				= cell_int.processedCell.singleCell;
 
 	// Instantiate the cell processor
 	CellProcessor IPCore(.ports(cell_int.cellPorts));
