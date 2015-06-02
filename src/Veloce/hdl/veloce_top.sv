edba35bc8014ec4d95987493d93b54c7d8f46808
bhunt2 <bhunt2@pdx.edu>
2015-06-01 22:31:10 -0700

    updated correct veloce_top

--- src/Veloce/hdl/veloce_top.sv
+++ src/Veloce/hdl/veloce_top.sv
@@ -1,11 +1,13 @@
 
 module veloce_top;
+
+	import CellProcessingPkg::*;
 	reg clk, rst;
-	reg [CellProcessingPkg::cellDepth - 1:0]	cellA;
-   reg [CellProcessingPkg::cellDepth - 1:0]	cellB;
-	reg CellProcessingPkg::userInput_t userInputA;
-	reg CellProcessingPkg::opcodes_t opcode;
-	reg CellProcessingPkg::pixel_t processedPixel;
+	bit [cellDepth - 1:0]	cellA;
+    bit [cellDepth - 1:0]	cellB;
+	bit userInput_t userInputA;
+	bit opcodes_t opcode;
+	bit pixel_t processedPixel;
 	integer done;
 
 	//clock generator
@@ -29,9 +31,9 @@ module veloce_top;
 
 	//DPI import functions
 	import "DPI-C" task load_images();
-	import "DPI-C" task send_3x3_pixels(output [CellProcessingPkg::cellDepth - 1:0] cellA,
-													output [CellProcessingPkg::cellDepth - 1:0] cellB);
-	import "DPI-C" task recieve_result_pixel(input CellProcessingPkg::pixel_t processedPixel, output bit [31:0] done);
+	import "DPI-C" task send_3x3_pixels(output [cellDepth - 1:0] cellA,
+													output [cellDepth - 1:0] cellB);
+	import "DPI-C" task recieve_result_pixel(input pixel_t processedPixel, output bit [31:0] done);
 	import "DPI-C" task shutdown_hvl();
 	
 	cellProcessor_int cell_int(clk, rst);
--- src/veloce_top.sv
+++ src/veloce_top.sv
@@ -1,69 +1,19 @@
 
-module veloce_top;
-	reg clk, rst;
-	reg [CellProcessingPkg::cellDepth - 1:0]	cellA;
-   reg [CellProcessingPkg::cellDepth - 1:0]	cellB;
-	reg CellProcessingPkg::userInput_t userInputA;
-	reg CellProcessingPkg::opcodes_t opcode;
-	reg CellProcessingPkg::pixel_t processedPixel;
-	integer done;
-
-	//clock generator
-	//tbx clkgen
-	initial
-	begin
-		clk = 0;
-		forever
-		begin
-			#10 clk = ~clk;
-		end
-	end
-
-	//reset generator
-	//tbx clkgen
-	initial
-	begin
-		rst = 1;
-		#20 rst = 0;
-	end
-
-	//DPI import functions
-	import "DPI-C" task load_images();
-	import "DPI-C" task send_3x3_pixels(output [CellProcessingPkg::cellDepth - 1:0] cellA,
-													output [CellProcessingPkg::cellDepth - 1:0] cellB);
-	import "DPI-C" task recieve_result_pixel(input CellProcessingPkg::pixel_t processedPixel, output bit [31:0] done);
-	import "DPI-C" task shutdown_hvl();
+module veloce_top
+(
+	input  clk,
+	input  rst,
+    input  [CellProcessingPkg::cellDepth - 1:0]	cellA,
+    input  [CellProcessingPkg::cellDepth - 1:0]	cellB,
+    input  CellProcessingPkg::userInput_t 		userInputA,
+    input  CellProcessingPkg::opcodes_t 		opcode,
+    output CellProcessingPkg::pixel_t			processedPixel
+);
 	
 	cellProcessor_int cell_int(clk, rst);
-
 	
-	initial
-	begin
-		@(posedge clk);
-		while(rst) @(posedge clk);
-		load_images();
-	end
+	
 
-	//Process the cells
-	always @(posedge clk)
-	begin
-		if(!rst)
-		begin
-			send_3x3_pixels(cellA, cellB);
-			@(posedge clk);
-			@(posedge clk);
-			@(posedge clk);
-			@(posedge clk);
-			recieve_result_pixel(processedPixel, done);
-
-			if(done)
-			begin
-				shutdown_hvl();
-				$finish();
-			end
-		end
-	end
-end
 	
 	assign cell_int.cellA		= cellA;
 	assign cell_int.cellB		= cellB;
